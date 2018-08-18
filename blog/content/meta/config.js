efaa8f04a3224d035c410c0dc3b5dadf613ddc1a
Rishabh Chakrabarti <rishabh.chakrabarti@protonmail.com>
2018-08-19 00:16:36 +0530

    fixed prefix paths

--- blog/content/meta/config.js
+++ blog/content/meta/config.js
@@ -5,7 +5,7 @@ module.exports = {
   shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
   siteDescription: "PersonalBlog is a GatsbyJS starter.",
   siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
-  pathPrefix: "/rishacha-blog",
+  pathPrefix: `/rishacha-blog`,
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -33,7 +33,8 @@ module.exports = {
     title: config.siteTitle,
     description: config.siteDescription,
     siteUrl: config.siteUrl,
-    pathPrefix: config.pathPrefix,
+    // pathPrefix: config.pathPrefix,
+    pathPrefix: `/rishacha-blog`,
     algolia: {
       appId: process.env.ALGOLIA_APP_ID ? process.env.ALGOLIA_APP_ID : "",
       searchOnlyApiKey: process.env.ALGOLIA_SEARCH_ONLY_API_KEY
